3a3cf3d157ca1eeb26da58bccf2325e5d6f51554
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-11-13 19:42:35 -0600

    adding test component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
 
+// stateless functional component
+// always return JSX
+
+function Greeting() {
+  return <div>This is Dylan yoooooooo</div>;
+}
+
+ReactDom.render(<Greeting />, document.getElementById("root"));
+
+export default Greeting;
