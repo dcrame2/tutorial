efdfd7846821b1c3f27116a74d83f7ef1622b1b7
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-11-19 08:49:55 -0600

    new components

--- src/index.js
+++ src/index.js
@@ -9,36 +9,24 @@ function BookList() {
   return (
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
+const author = "BY JORDAN B. PETERSON";
 const Book = () => {
+  const title = "12 Rules for Life";
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/I/71qOKP4LHRL._SX140_.jpg"
+        alt=""
+      />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
-
-const Image = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/I/71qOKP4LHRL._SX140_.jpg"
-      alt=""
-    />
-  );
-};
-
-const Title = () => <h1>12 Rules for Life</h1>;
-const Author = () => <h6>BY JORDAN B. PETERSON</h6>;
 
 ReactDom.render(<BookList />, document.getElementById("root"));
 
