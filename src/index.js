1545fd1817af0e7a751167685e8d63498d91d4c6
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-11-19 08:18:56 -0600

    push to github

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 import React from "react";
 import ReactDom from "react-dom";
+import "./index.css";
 
 // stateless functional component
 // always return JSX
 
-function Greeting() {
-  return <div>This is Dylan yoooooooo</div>;
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
-export default Greeting;
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/71qOKP4LHRL._SX140_.jpg"
+      alt=""
+    />
+  );
+};
+
+const Title = () => <h1>12 Rules for Life</h1>;
+const Author = () => <h6>BY JORDAN B. PETERSON</h6>;
+
+ReactDom.render(<BookList />, document.getElementById("root"));
+
+export default BookList;
