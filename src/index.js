96f09e5cecb0e2d0854ef217a731a8aa8bb23843
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2022-11-19 16:28:03 -0600

    event handlers

--- src/index.js
+++ src/index.js
@@ -38,12 +38,33 @@ function BookList() {
 }
 
 const Book = ({ img, title, author }) => {
-  // const { img, title, author } = props;
+  // Attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("Hello world");
+  };
+
+  const complexExample = (author) => {
+    console.log(author);
+  };
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt="book" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        Book
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
+      </button>
     </article>
   );
 };
